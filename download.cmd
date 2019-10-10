c8f09d65fac0f482531873e9bcdeb3cd021afa29
mohs <sldbcbi@sohu.com>
2019-10-10 09:57:50 +0800

    动态

--- download.cmd
+++ download.cmd
@@ -1,17 +1,13 @@
-@echo off
+@echo on
 set u=https://github.com/JackMohs/libraries/blob/master/Picture
-set /a j=%1-1
+set /a a=%1
 :begin
+mkdir %a%
+set /a j=1
+:while
+wget -nc -P .\%a% %u%/Fonts/%j%.jpg 2>&1|findstr "404" >nul && goto break
 set /a j=%j%+1
-
-if %j% GTR %2 goto end
-
-mkdir %j% || echo 子目录或文件 %j% 已经新建。
-
-for /l %%i in (10,1,35) do (	
-	wget -nc -P .\%j% %u%/Fonts/%%i.jpg 2>&1|find "404" >nul && goto begin
-)
-
-echo ERROR LESS
-
-:end
+goto while
+:break
+if %a% lss %2 (set /a a=%a%+1 & goto begin)
+pause
